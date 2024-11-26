89b004e376bd145f56da70fe3278ea0ec6c91d6b
Harvar3105 <Petrochenko03@inbox.ru>
2024-11-27 00:38:27 +0200

    logging changes

--- src/main/java/app/configs/security/RequestFilter.java
+++ src/main/java/app/configs/security/RequestFilter.java
@@ -8,6 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Enumeration;
 
 @Log4j2
@@ -15,25 +16,26 @@ public class RequestFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-//        log.warn("Request URI: {}", request.getRequestURI());
-//        log.warn("HTTP Method: {}", request.getMethod());
-        log.warn("NEW REQUEST HEADER:\n");
-
         Enumeration<String> headerNames = request.getHeaderNames();
         if (headerNames != null) {
-            boolean thereIsAuthHeader = false;
             while (headerNames.hasMoreElements()) {
                 String headerName = headerNames.nextElement();
                 String headerValue = request.getHeader(headerName);
-                if (headerName.trim().equals("Authorization")){
-                    thereIsAuthHeader = true;
+                if (headerName.toLowerCase().contains("Authorization".toLowerCase())){
                     log.warn("Header: {} = {}", headerName, headerValue);
                 }
             }
-            if (!thereIsAuthHeader){
-                log.warn("No Authorization header found!");
+        }
+        log.warn("Request ended!!!!! \n");
+
+        Collection<String> responseHeaderNames = response.getHeaderNames();
+        if (responseHeaderNames != null) {
+            for (var header : responseHeaderNames){
+                String value = response.getHeader(header);
+                log.warn("Header: {} = {}", header, value);
             }
         }
+        log.warn("Response ended!!!!! \n");
 
         filterChain.doFilter(request, response);
     }
